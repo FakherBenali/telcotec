bb60d39779a3559e627d21a2a4a3ad61a255116a
AymenGhazouani <aymeeen.gh@gmail.com>
2024-07-31 13:33:31 +0100

    dashboard minor changes

--- front-end/src/app/home/home.component.ts
+++ front-end/src/app/home/home.component.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
+import { Component,signal } from '@angular/core';
 import { CoverComponent } from './cover/cover.component';
 import { NavbarModule } from '../navbar/navbar.module';
 import { ChartComponent } from '../chart/chart.component';
 import {RevenuesComponent} from './../revenues/revenues.component'
+import {MatExpansionModule} from '@angular/material/expansion';
+import {ChangeDetectionStrategy} from '@angular/core';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CoverComponent,NavbarModule,ChartComponent, RevenuesComponent ],
+  imports: [CoverComponent,NavbarModule,ChartComponent, RevenuesComponent,MatExpansionModule ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
+
 })
 export class HomeComponent {
+  readonly panelOpenState = signal(false);
 
 }
